444c9fda5e201e4ab6f0573bfc4747d34d17c96d
Shiva180201 <124235692+Shiva180201@users.noreply.github.com>
2023-10-06 13:48:25 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,31 @@
+// 268Missing number
+// https://leetcode.com/problems/missing-number/
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {9,6,4,2,3,5,7,0,1};
+        int ans = sort(arr);
+        System.out.println(ans);
+    }
+    public static int sort(int[] arr) {
+        int index = 0;
+        while (index < arr.length) {
+            if (arr[index] < arr.length && arr[index] != arr[arr[index]]) {
+                swap(arr,index,arr[index]);
+            }
+            else {
+                index++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != i) {
+                return i;
+            }
+        }
+        return arr.length;
+    }
+    public static void swap(int[] arr,int first,int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+}
